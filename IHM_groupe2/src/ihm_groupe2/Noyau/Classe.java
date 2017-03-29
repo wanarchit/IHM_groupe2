78820d49dcf7bc516594169b9c7ad94e4ec2202d
Paul <Paul@DESKTOP-1U9DTNA>
2017-03-29 17:10:23 +0200

    Noyau + tortues

--- IHM_groupe2/src/ihm_groupe2/Noyau/Classe.java
+++ IHM_groupe2/src/ihm_groupe2/Noyau/Classe.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ihm_groupe2.Noyau;
+
+import java.util.ArrayList;
 
 /**
  *
- * @author Paul
+ * @author Marin
  */
 public class Classe {
+    private String nomClasse;
+    private ArrayList<Eleve> listeEleve;
+    private Professeur professeur;
     
+    
+    public Classe(String nomCl,Professeur leProf){
+        nomClasse=nomCl;
+        professeur=leProf;
+    }
+    
+    public void ajoutEleve(Eleve monEleve){
+        listeEleve.add(monEleve);
+    }
+    
+    public void suppEleve(Eleve monEleve){
+        for(Eleve el:listeEleve){
+            if (el.equals(monEleve)){
+                listeEleve.remove(el);
+            }
+        }
+    }
+    
+    public
 }
