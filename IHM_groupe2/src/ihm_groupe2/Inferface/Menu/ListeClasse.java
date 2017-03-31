44c7d398cd4a0debe43385ad89d1493c10d6fb62
HeyJoLetsGo <jonathan.rivault@hotmail.fr>
2017-03-31 14:49:46 +0200

    Wouah trop de changements !

--- IHM_groupe2/src/ihm_groupe2/Inferface/Menu/ListeClasse.java
+++ IHM_groupe2/src/ihm_groupe2/Inferface/Menu/ListeClasse.java
@@ -1,9 +1,12 @@
 package ihm_groupe2.Inferface.Menu;
 
+import javax.swing.JTable;
+
 /**
  *
  * @author Jonathan
  */
 public class ListeClasse {
     //Ici c'est un JTable avec toutes les classes
+    private JTable listeClasses = new JTable();
 }
