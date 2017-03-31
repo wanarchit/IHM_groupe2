809ad055477f614a6fc444874587484e9f9c45a8
HeyJoLetsGo <jonathan.rivault@hotmail.fr>
2017-03-31 14:55:08 +0200

    bl

--- IHM_groupe2/src/ihm_groupe2/Inferface/Menu/ListeClasse.java
+++ IHM_groupe2/src/ihm_groupe2/Inferface/Menu/ListeClasse.java
@@ -7,6 +7,6 @@ import javax.swing.JTable;
  * @author Jonathan
  */
 public class ListeClasse {
-    //Ici c'est un JTable avec toutes les classes
+    //Ici c'est un JTable avec toutes les classes du truc
     private JTable listeClasses = new JTable();
 }
