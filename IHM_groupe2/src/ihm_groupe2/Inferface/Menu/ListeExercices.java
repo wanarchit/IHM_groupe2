ae1ac837c30135438a9cfa35c06214db6b61d0e1
Delphine <Delphine@ASUS-Delphine>
2017-04-03 12:04:17 +0200

    Création du JTable pour la liste des exercices : classe modèle + classe tableau + création d'une lasse controleur pas encore remplie + création d'un main test (FrameTest) DP

--- IHM_groupe2/src/ihm_groupe2/Inferface/Menu/ListeExercices.java
+++ IHM_groupe2/src/ihm_groupe2/Inferface/Menu/ListeExercices.java
@@ -1,20 +1,47 @@
 package ihm_groupe2.Inferface.Menu;
 
-import javax.swing.JPanel;
-import javax.swing.JTree;
+import ihm_groupe2.Modele.TableExercices;
+import ihm_groupe2.Noyau_fonctionnel.*;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 
 /**
  * Classe permettant d'afficher les exercices existants
- * @author Marin
+ * @author Delphine
  */
-public class ListeExercices extends JPanel{
-    private JTree listeExercices = new JTree();
-
-
+public class ListeExercices extends JScrollPane{
+    private JTable tableExercices;
+    private TableExercices modeleTable;
+    
     /**
      * Constructeur de listeExercices
      */
     public ListeExercices(){
+        ArrayList<Exercice> maListe = new ArrayList();
+        ImageIcon monImage = new ImageIcon(); //récupérer l'url de l'icone
         
+        //Pour les tests création de ma liste ici sera envoyé via l'application prof
+        Exercice ex = new Exercice ("Exercice1", "Comm", 1, monImage);
+        maListe.add(ex);
+        
+        for (Exercice e : maListe){
+            modeleTable = new TableExercices(e);
+            tableExercices = new JTable (modeleTable);
+            
+            tableExercices.setRowSelectionAllowed(true);
+            tableExercices.setCellSelectionEnabled(true);
+            tableExercices.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        }
+        this.add(tableExercices);
+        this.setViewportView(tableExercices);
     }
+    
+    public void setData (Exercice e){
+        modeleTable.setData(e);
+        modeleTable.fireTableDataChanged();
+    }
+    
 }
