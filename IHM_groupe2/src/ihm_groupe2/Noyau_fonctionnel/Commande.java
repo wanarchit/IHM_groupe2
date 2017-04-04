25d32a48cffab25051b03b476c872b57cac91c51
HeyJoLetsGo <jonathan.rivault@hotmail.fr>
2017-04-04 16:00:48 +0200

    Noyau fini

--- IHM_groupe2/src/ihm_groupe2/Noyau_fonctionnel/Commande.java
+++ IHM_groupe2/src/ihm_groupe2/Noyau_fonctionnel/Commande.java
@@ -7,7 +7,9 @@ package ihm_groupe2.Noyau_fonctionnel;
 public class Commande {
     
     private String nomCommande;
-    private TortueG tortue;
+    private TortueG tortG;
+    private TortueRapide tortR;
+    private TortueCouleur tortC;
     
     /**
      * Constructeur de commande
@@ -16,7 +18,17 @@ public class Commande {
      */
     public Commande(String nomCom, TortueG t){
         nomCommande = nomCom;
-        tortue = t;
+        tortG = t;
+    }
+    
+    public Commande(String nomCom, TortueRapide t){
+        nomCommande = nomCom;
+        tortR = t;
+    }
+    
+    public Commande(String nomCom, TortueCouleur t){
+        nomCommande = nomCom;
+        tortC = t;
     }
     
     /**
@@ -32,7 +44,7 @@ public class Commande {
      * @return Le type de tortue utilisée
      */
     public TortueG getTortue() {
-        return tortue;
+        return tortG;
     }
     
     /**
@@ -40,6 +52,47 @@ public class Commande {
      * @param c la commande à rejouer
      */
     public void doAction(Commande c) {
-        
+        switch(c.getCommande()){
+            case "Avancer":
+                c.getTortue().avancer();
+            case "Tourner":
+                c.getTortue().tourner();
+            case "Ecrire":
+                c.getTortue().tracer(true);
+            case "Pas ecrire":
+                c.getTortue().tracer(false);
+            case "Accélérer":
+                if (c.getTortue() instanceof TortueRapide){
+                    ((TortueRapide) c.getTortue()).accelerer();
+                }
+            case "Ralentir":
+                if (c.getTortue() instanceof TortueRapide){
+                    ((TortueRapide) c.getTortue()).ralentir();
+                }
+            case "Rouge":
+                if (c.getTortue() instanceof TortueCouleur){
+                    ((TortueCouleur) c.getTortue()).setCouleur("red");
+                }
+            case "Bleu":
+                if (c.getTortue() instanceof TortueCouleur){
+                    ((TortueCouleur) c.getTortue()).setCouleur("blue");
+                }
+            case "Magenta":
+                if (c.getTortue() instanceof TortueCouleur){
+                    ((TortueCouleur) c.getTortue()).setCouleur("magenta");
+                }
+            case "Noir":
+                if (c.getTortue() instanceof TortueCouleur){
+                    ((TortueCouleur) c.getTortue()).setCouleur("black");
+                }
+            case "Jaune":
+                if (c.getTortue() instanceof TortueCouleur){
+                    ((TortueCouleur) c.getTortue()).setCouleur("yellow");
+                }
+            case "Vert":
+                if (c.getTortue() instanceof TortueCouleur){
+                    ((TortueCouleur) c.getTortue()).setCouleur("green");
+                }
+        }
     }
 }
