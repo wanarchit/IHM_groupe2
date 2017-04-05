8e970fb5b5904b5dae7390890e5b1c6949805654
Delphine <Delphine@ASUS-Delphine>
2017-04-05 15:57:05 +0200

    Ca y est l'action sur le tableau fonctionne --> pour l'instant ouvre dans une nouvelle fenêtre DP

--- IHM_groupe2/src/ihm_groupe2/Controleur/CtrlTableExercices.java
+++ IHM_groupe2/src/ihm_groupe2/Controleur/CtrlTableExercices.java
@@ -1,8 +1,10 @@
 package ihm_groupe2.Controleur;
 
 import ihm_groupe2.Inferface.Menu.ListeExercices;
+import ihm_groupe2.Inferface.Menu.PanelModifExo;
+import ihm_groupe2.Modele.TableExercices;
 import ihm_groupe2.Noyau_fonctionnel.Exercice;
-import javax.swing.JTable;
+import javax.swing.JFrame;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -17,10 +19,29 @@ public class CtrlTableExercices implements ListSelectionListener {
             
     public CtrlTableExercices(ListeExercices myTable){
         this.myTable = myTable;
+
     }
     @Override
-    public void valueChanged(ListSelectionEvent e) {
-        Exercice e1 = (Exercice) myTable.getJTable().getValueAt(myTable.getJTable().getSelectedRow(),2);
-        //appeller la méthode pour basuler vers la fiche de modification de l'exercice e1
-    }
-}
+    public void valueChanged(ListSelectionEvent act) {
+
+        TableExercices modelTable = (TableExercices) myTable.getModele();
+        if (act.getValueIsAdjusting())
+                return;
+        ListSelectionModel lsm = (ListSelectionModel)act.getSource();
+        if (!lsm.isSelectionEmpty()) {
+
+            int selectedRow = lsm.getMinSelectionIndex();
+            Exercice e = modelTable.getExoRow(selectedRow);
+            PanelModifExo pan = new PanelModifExo(e);
+
+            JFrame theFram = new JFrame();
+            theFram.add(pan);
+            theFram.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            theFram.setTitle("Application gestion exercices");  
+            theFram.setSize(1000, 600);
+            theFram.setLocationRelativeTo(null);
+            theFram.setVisible(true);
+        }
+    }   
+}   
+
